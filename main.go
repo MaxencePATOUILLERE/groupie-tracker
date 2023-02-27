33dfcca2daabec0e6fdfefa07c69d82db5703eb3
Maxence PATOUILLERE <maxence.patouillere@ynov.com>
2023-02-27 16:17:40 +0100

    add function to convert json in struct for dates locations & artists. create struct for artists dates & locations.

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api")
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		w.Write(data)
-	}
+type Artists struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type Dates struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type Locations struct {
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
 }
 
 func artists(w http.ResponseWriter, r *http.Request) {
@@ -26,42 +41,41 @@ func artists(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func locations(w http.ResponseWriter, r *http.Request) {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+func getArtist(id int) Artists {
+	var artist Artists
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists/" + strconv.Itoa(id))
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		w.Write(data)
+		// Gérer l'erreur
 	}
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&artist)
+	return artist
 }
 
-func dates(w http.ResponseWriter, r *http.Request) {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
+func getLocation(id int) Locations {
+	var location Locations
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/locations/" + strconv.Itoa(id))
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		w.Write(data)
+		// Gérer l'erreur
 	}
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&location)
+	return location
 }
 
-func relation(w http.ResponseWriter, r *http.Request) {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+func getDate(id int) Dates {
+	var date Dates
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/dates/" + strconv.Itoa(id))
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		w.Write(data)
+		// Gérer l'erreur
 	}
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&date)
+	return date
 }
+
 func main() {
-	http.HandleFunc("/api", homePage)
-	http.HandleFunc("/api/artists", artists)
-	http.HandleFunc("/api/locations", locations)
-	http.HandleFunc("/api/dates", dates)
-	http.HandleFunc("/api/relation", relation)
-
+	http.HandleFunc("/", artists)
 	fmt.Println("Starting server...")
 	http.ListenAndServe(":8080", nil)
 }
